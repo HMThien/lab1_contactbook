2f4598af6198cc52099ed190110c55421ff98f77
HM Thien <thiendt20v7x081@dttx.ctu.edu.vn>
2022-06-13 23:50:13 +0700

    Sua file

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/",(req, res) => {
+    res.json({message:"Wellome - Chào mừng"});
+});
+module.exports = app;
+
+app.use("/api/contacts", contactsRouter);
+module.exports = app;
