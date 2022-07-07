174d42741bd517246518fee3bfc331a85e03a96d
HM_Thien <hmthien1112ntn@gmail.com>
2022-07-07 21:25:17 +0700

    Save

--- config/index.js
+++ config/index.js
@@ -2,5 +2,8 @@ const config = {
     app: {
     port: 3000,
     },
+    db: {
+        uri: process.env.MONGODB_URI || "mongodb:////127.0.0.1:27017/lab2_contactbook"
+    }
     };
     module.exports = config;    
