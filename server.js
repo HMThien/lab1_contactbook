f7ef73e5625bea7fe08b678e96f66f4bf6bbcb40
HM_Thien <hmthien1112ntn@gmail.com>
2022-07-07 23:11:43 +0700

    Save

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
+// lab1
+//const app = require("./app");
+//const config = require("./config");
+//const PORT = config.app.port;
+//app.listen(PORT, () => {
+//    console.log(`Server is running on port ${PORT}`);
+//});
+
+//lab2
 const app = require("./app");
 const config = require("./config");
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const MongoDB = require("./utils/mongodb.util");
+async function stastServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Ket noi thanh cong den MongoDN")
+
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server dang chay o cong ${PORT}`)
+        });
+
+    } catch (error) {
+        console.log("Khong kn dc MongoDB!", error);
+        process.exit();
+    }
+}
+
+startServer();
